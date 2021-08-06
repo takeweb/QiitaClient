f5b77a6e25bb1ed64ffe068505ba5106ca0db363
Taketomo Ohishi <takeweb@mac.com>
2021-08-06 09:02:36 +0900

    bindToLifecycleとプログレスバー追加

--- app/src/main/kotlin/com/example/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/com/example/qiitaclient/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.qiitaclient
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.ProgressBar
 import com.example.qiitaclient.client.ArticleClient
-import com.example.qiitaclient.model.Article
-import com.example.qiitaclient.model.User
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity
 import com.trello.rxlifecycle.kotlin.bindToLifecycle
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
@@ -17,10 +17,22 @@ import retrofit2.converter.gson.GsonConverterFactory
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : RxAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.list_view)
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+        val queryEditText = findViewById<EditText>(R.id.query_edit_text)
+        val searchButton = findViewById<Button>(R.id.search_button)
+
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listView.adapter = listAdapter
+        listView.setOnItemClickListener { adapterView, view, position, id ->
+            val article = listAdapter.articles[position]
+            ArticleActivity.intent(this, article).let { startActivity(it) }
+        }
 
         val gson = GsonBuilder()
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
@@ -33,14 +45,17 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         val articleClient = retrofit.create(ArticleClient::class.java)
-        val queryEditText = findViewById<EditText>(R.id.query_edit_text)
-        val searchButton = findViewById<Button>(R.id.search_button)
-        val listAdapter = ArticleListAdapter(applicationContext)
 
         searchButton.setOnClickListener{
+            progressBar.visibility = View.INVISIBLE
+
             articleClient.search(queryEditText.text.toString())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
+                .doAfterTerminate {
+                    progressBar.visibility = View.GONE
+                }
+                .bindToLifecycle(this)
                 .subscribe({
                     queryEditText.text.clear()
                     listAdapter.articles = it
@@ -50,26 +65,5 @@ class MainActivity : AppCompatActivity() {
                     toast("エラー: $it")
                 })
         }
-
-//        listAdapter.articles = listOf(
-//            dummyArticle("Kotlin入門", "たろう"),
-//            dummyArticle("Java入門", "じろう")
-//        )
-
-        val listView: ListView = findViewById(R.id.list_view) as ListView
-        listView.adapter = listAdapter
-        listView.setOnItemClickListener { adapterView, view, position, id ->
-            val article = listAdapter.articles[position]
-            ArticleActivity.intent(this, article).let { startActivity(it) }
-        }
     }
-
-    // ダミー記事
-    private fun dummyArticle(title: String, userName: String): Article =
-        Article(
-            id = "",
-            title = title,
-            url = "https://kotlinlang.org",
-            user = User(id = "", name = userName, profileImageUrl = "")
-        )
 }
